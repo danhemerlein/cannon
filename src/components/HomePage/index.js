ff91b62f2136e64ac0927108b06a69d945d1445c
Dan <danhemerlein@gmail.com>
2019-04-13 18:46:13 -0400

    add bg image and svg backgrounds

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -3,12 +3,24 @@ import React, { Component } from "react";
 import './HomePage.scss'
 import cx from 'classnames';
 
-export default class HomePage extends Component {
+import RainCloud from "components/Icons/RainCloud";
+import Storm from "components/Icons/Storm";
 
+
+export default class HomePage extends Component {
+  componentDidMount() {
+    
+  }
+  
   render() {
     return (
-      <div className={cx("HomePage overflow-hidden bg-color-red w100 vh100 p1")}>
-        This is the homepage component 
+      <div className={cx("HomePage overflow-hidden w100 vh100 ")}>
+        <div style={{ backgroundImage: "url('/assets/cannon.jpg')" }} className={cx("HomePage__image1 h100 bg-cover overflow-hidden flex justify-between")}>
+          <div className={cx("HomePage__svg-container")}>
+            <RainCloud></RainCloud>
+            <Storm></Storm>
+          </div>
+        </div>
       </div>
     )
   }
