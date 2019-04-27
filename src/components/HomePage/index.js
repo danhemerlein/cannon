d5686156566fac5e83a4e5d50842a6dfb41cd727
Dan <danhemerlein@gmail.com>
2019-04-27 13:20:02 -0400

    stuff

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -3,14 +3,61 @@ import React, { Component } from "react";
 import './HomePage.scss'
 import cx from 'classnames';
 
+import WaterGlass from "components/Icons/WaterGlass";
+
 export default class HomePage extends Component {
 
     render() {
     return (
-      <div className={cx("HomePage overflow-hidden w100 vh100 ")}>
+      <div className={cx("HomePage overflow-hidden w100 vh100")}>
+        <div style={{ backgroundImage: "url('/assets/cannon.jpg')" }} className={cx("HomePage__image1 h100 bg-cover overflow-hidden flex items-center  justify-between flex-col relative")}>
+        
+          <div className={cx("mt2 relative")}>
+            <h1 className={cx("HomePage__headline-one text-center headline-serif color-burnt-yellow bold")}>CANNON</h1>
+          </div>
 
-        <div style={{ backgroundImage: "url('/assets/cannon.jpg')" }} className={cx("HomePage__image1 h100 bg-cover overflow-hidden flex justify-between")}>
+          <div className={cx("HomePage__headline-three--container relative")}>
+            <h3 className={cx("HomePage__headline-three text-center headline-serif color-burnt-yellow bold")}>WATER GLASS</h3>
 
+            <div id="glass-1" className={cx("HomePage__svg-container absolute")}>
+              <WaterGlass></WaterGlass>
+            </div>
+
+            <div id="glass-2" className={cx("HomePage__svg-container absolute")}>
+              <WaterGlass></WaterGlass>
+            </div>
+
+            <div id="glass-3" className={cx("HomePage__svg-container absolute")}>
+              <WaterGlass></WaterGlass>
+            </div>
+
+            <div id="glass-4" className={cx("HomePage__svg-container absolute")}>
+              <WaterGlass></WaterGlass>
+            </div>
+
+            <div id="glass-5" className={cx("HomePage__svg-container absolute")}>
+              <WaterGlass></WaterGlass>
+            </div>
+            
+          </div>
+
+          <div></div>
+
+          <div>
+            <ul className={cx("HomePage__footer-nav mb2")}>
+              <li className={cx("HomePage__footer-nav--list-item mx1 color-burnt-yellow inline-block")}>
+                <a href="https://distrokid.com/hyperfollow/cannon12/water-glass" target="_blank" rel="noopener noreferrer">LISTEN</a>
+              </li>
+
+              <li className={cx("HomePage__footer-nav--list-item mx1 color-burnt-yellow inline-block")}>
+                <a href="https://www.instagram.com/cannonfew/" target="_blank" rel="noopener noreferrer">INSTA</a>
+              </li>
+
+              <li className={cx("HomePage__footer-nav--list-item mx1 color-burnt-yellow inline-block")}>
+                <a href="https://twitter.com/cannon_few" target="_blank" rel="noopener noreferrer">TWITTER</a>
+              </li>
+            </ul>
+          </div>
 
         </div>
       </div>
