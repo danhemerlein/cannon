16d0ca9fb69cdf0f4795ec019ba269c1bf6ea3bf
Dan <danhemerlein@gmail.com>
2019-05-07 14:32:38 -0400

    fix field name

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -73,7 +73,7 @@ export default class HomePage extends Component {
             <SocialsModule 
               soundLinkCTA={this.props.soundLinkCTA}
               instagramLinkCTA={this.props.instagramLinkCTA}
-              twitterLinkCTA={this.props.twite}
+              twitterLinkCTA={this.props.twitterLinkCTA}
             />
 
           </div>
