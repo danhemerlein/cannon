be3eb54437202536d74c5f88d879c763f4021001
Dan <danhemerlein@gmail.com>
2020-04-02 21:59:33 -0400

    begin homepage redesign

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -61,63 +61,14 @@ export default class HomePage extends Component {
           className={cx("HomePage__image1 h100 flex flex-col relative")}
         >
           <div className="flex w100 justify-end p1 md:p2 flex-col items-end">
-            <MainLogo src={this.props.logo.fields.file.url}></MainLogo>
+            {/* <MainLogo src={this.props.logo.fields.file.url}></MainLogo> */}
 
-            <h1 className="HomePage__headline-one color-white futura mt2 md:mt0 text-center sm:text-right underline">
+            {/* <h1 className="HomePage__headline-one color-white futura mt2 md:mt0 text-center sm:text-right underline">
               <Link to="/merch">
                 “Words Are Important” <br></br>Long-Sleeve Tee<br></br>available now
               </Link>
-            </h1>
-
-            {/* <div className="HomePage__site-link">
-
-              <Link to="/t-shirt">
-
-                <div className={cx("HomePage__site-link flex flex-col color-white")}>
-                  <h4 className={cx("relative headline-serif")}>Limited Edition</h4>
-
-                  <div className={cx("HomePage__site-link--desktop")}>
-                    <h4 className={cx("relative headline-serif")}>"These Walls" T-Shirt</h4>
-                  </div>
-
-                  <div className={cx("HomePage__site-link--mobile")}>
-
-                    <h4 className={cx("relative headline-serif")}>"These Walls"</h4>
-                    <h4 className={cx("relative headline-serif")}>T-Shirt</h4>
-
-                  </div>
-
-                </div>
-
-              </Link>
-
-            </div> */}
+            </h1>   */}
           </div>
-
-          {/* <div className={cx("HomePage__headline-center-container flex justify-center flex-col text-center mt8 mb4 p1 md:p2")}>
-            <h3 className={cx("HomePage__headline-three headline-serif bold color-white inline-block")}>
-              <a href="https://open.spotify.com/track/3o2qklLlYRiHUYByBfoFl5?si=yeaTZeqOQI-q6qegdjMiLA" target="_blank" rel="noopener noreferrer">
-                {this.props.footerTitle}
-              </a>
-            </h3>
-
-            <h6 className={cx("HomePage__headline-six headline-serif color-white bold inline-block")}>
-              <a href="https://open.spotify.com/track/3o2qklLlYRiHUYByBfoFl5?si=yeaTZeqOQI-q6qegdjMiLA" target="_blank" rel="noopener noreferrer">
-                Lyric Video Out Now
-              </a>
-            </h6>
-          </div> */}
-
-          {/* <div className="HomePage__container-iframe w100 flex justify-center">
-
-            <iframe className="col-12 p1 md:p2 mb8" title="These Walls Lyric Video" src="https://www.youtube.com/embed/wNESoi5Ty2Q" frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen="allowfullscreen"
-              mozallowfullscreen="mozallowfullscreen"
-              msallowfullscreen="msallowfullscreen"
-              oallowfullscreen="oallowfullscreen"
-              webkitallowfullscreen="webkitallowfullscreen"></iframe>
-
-          </div> */}
 
           <div
             className={cx(
