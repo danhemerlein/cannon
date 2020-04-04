a07ad84dd1d2724293fe517d237d91bc739abf53
Dan <danhemerlein@gmail.com>
2020-04-04 14:40:51 -0400

    small updates to the merch page

--- src/components/MerchPage/index.js
+++ src/components/MerchPage/index.js
@@ -87,14 +87,64 @@ export default class MerchPage extends Component {
                 {/* <p>sold out</p> */}
               </h4>
 
+              <div
+                className={cx(
+                  "MerchPage__merch-photo-container col-12 px1 md:px2 flex justify-center items-center mb2 flex-col md:flex-row"
+                )}
+              >
+                <div
+                  className={cx(
+                    "MerchPage__merch-photo bg-color-white col-12 md:col-4 mr0 mb1 md:mb0 relative md:mr1"
+                  )}
+                >
+                  <img
+                    className={cx("col-12")}
+                    src={this.props.merchPrimary.fields.file.url}
+                    alt=""
+                  />
+                  <p className="MerchPage__merch-photo-paragraph text-center color-black pb_5">
+                    front
+                  </p>
+
+                  {/* <div className="MerchPage__sold-out-message-two headline-serif color-white absolute px1 py_5 md:px2 md:py1">
+                sold out
+              </div> */}
+                </div>
+
+                <div
+                  className={cx(
+                    "MerchPage__merch-photo MerchPage__merch-photo--back bg-color-white col-12 md:col-4 ml0 relative"
+                  )}
+                >
+                  <img
+                    className={cx("col-12")}
+                    src={this.props.merchSecondary.fields.file.url}
+                    alt=""
+                  />
+
+                  <p className="MerchPage__merch-photo-paragraph text-center color-black pb_5">
+                    back
+                  </p>
+
+                  {/* <div className="MerchPage__sold-out-message-two headline-serif color-white absolute px1 py_5 md:px2 md:py1">
+                sold out
+              </div> */}
+                </div>
+              </div>
+
               <div className={cx("col-12 md:col-8")}>
                 <h4
                   className={cx(
                     "MerchPage__merch-headline headline-serif color-white pb1"
                   )}
                 >
-                  <span className="block md:none">To complete the order, <br></br>please include in your email:{" "}</span>
-                  <span className="none md:block">To complete the order, please include in your email:{" "}</span>
+                  <span className="block md:none">
+                    To complete the order, <br></br>please include in your
+                    email:{" "}
+                  </span>
+                  <span className="none md:block">
+                    To complete the order, please include in your email:{" "}
+                  </span>
                 </h4>
 
                 <ul
@@ -102,17 +152,25 @@ export default class MerchPage extends Component {
                     "MerchPage__merch-headline MerchPage__merch-unordered-list headline-serif color-white pb2"
                   )}
                 >
-                  <li className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}>
+                  <li
+                    className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}
+                  >
                     Size and quantity of shirts (S, M, L, XL)
                   </li>
-                  <li className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}>
+                  <li
+                    className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}
+                  >
                     Your shipping address
                   </li>
-                  <li className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}>
+                  <li
+                    className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}
+                  >
                     Venmo Luke Doyle (@overeasymusic) total amount of order
                     ($25/shirt + $5 shipping & handling)
                   </li>
-                  <li className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}>
+                  <li
+                    className={cx("text-left w100 ml2 my1 md:py0 pr2 md:pr0")}
+                  >
                     Upon receipt, order will be shipped in 3-5 business days
                     (email confirmation)
                   </li>
@@ -123,58 +181,14 @@ export default class MerchPage extends Component {
 
           <div
             className={cx(
-              "MerchPage__merch-photo-container col-12 px1 md:px2 flex justify-center items-center mb8 flex-col"
-            )}
-          >
-            <div
-              className={cx(
-                "MerchPage__merch-photo bg-color-white col-6 md:col-4 mr0 mb1 relative"
-              )}
-            >
-              <img
-                className={cx("col-12")}
-                src={this.props.merchPrimary.fields.file.url}
-                alt=""
-              />
-              <p className="MerchPage__merch-photo-paragraph text-center color-black pb_5">
-                front
-              </p>
-
-              {/* <div className="MerchPage__sold-out-message-two headline-serif color-white absolute px1 py_5 md:px2 md:py1">
-                sold out
-              </div> */}
-            </div>
-
-            <div
-              className={cx(
-                "MerchPage__merch-photo MerchPage__merch-photo--back bg-color-white col-6 md:col-4 ml0 relative"
-              )}
-            >
-              <img
-                className={cx("col-12")}
-                src={this.props.merchSecondary.fields.file.url}
-                alt=""
-              />
-
-              <p className="MerchPage__merch-photo-paragraph text-center color-black pb_5">
-                back
-              </p>
-
-              {/* <div className="MerchPage__sold-out-message-two headline-serif color-white absolute px1 py_5 md:px2 md:py1">
-                sold out
-              </div> */}
-            </div>
-          </div>
-
-          <div
-            className={cx(
               "HomePage__social-toggle absolute",
               {
-                "HomePage__social-toggle--nav-open": this.state.navOpen === true
+                "HomePage__social-toggle--nav-open":
+                  this.state.navOpen === true,
               },
               {
                 "HomePage__social-toggle--nav-closed":
-                  this.state.navOpen === false
+                  this.state.navOpen === false,
               }
             )}
           >
@@ -189,11 +203,11 @@ export default class MerchPage extends Component {
               "HomePage__social-container HomePage__social-container--mobile self-end absolute",
               {
                 "HomePage__social-container--mobile--show":
-                  this.state.navOpen === true
+                  this.state.navOpen === true,
               },
               {
                 "HomePage__social-container--mobile--hide":
-                  this.state.navOpen === false
+                  this.state.navOpen === false,
               }
             )}
           >
