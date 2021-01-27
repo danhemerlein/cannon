0d87af6b7a0e518c6762bda53cb918b15e745ce8
Dan Hemerlein <danhemerlein@gmail.com>
2021-01-26 22:16:54 -0500

    stuff

--- src/components/BleuTapes/index.js
+++ src/components/BleuTapes/index.js
@@ -1,51 +1,40 @@
 import React, { Component } from "react";
+import { useMediaQuery } from "react-responsive";
+import breakpoints from "utils/breakpoints";
+
+
 import './styles.scss'
 
-export default class BleuTapes extends Component {
+const BleuTapes = (props) => {
 
-  render() {
-    const backgroundImage = {
-      backgroundImage: "url('/assets/bleu-tapes.png')",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    };
-    const backgroundImageInner = {
-      backgroundImage: "url('/assets/bleu-tracks.png')",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-    };
-    return (
-      <a
-        href="https://open.spotify.com/album/0p6BK5gxpxydQPG5HsyOIl?si=WUH1JklSRBmXB7x3AgZdKQ"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="BleuTapes p1" style={backgroundImage}>
-          {/*
-          <div className="h100 w100 relative ocr">
-            <div>
-              <h2>1. youngest child (2:15)</h2>
-              <p>(C. Few)</p>
-              <p>produced by: Cannon Few</p>
-              <p>mixed by: Cannon Few</p>
-              <p>mastered by: Tom Waltz</p>
-            </div>
+  const isTabletUp = useMediaQuery({
+    query: breakpoints.tablet,
+  });
 
-            <div>
-              <h2>1. backpedal (2:01)</h2>
-              <p>(C. Few, L. Doyle, T. Kellan)</p>
-              <p>produced by: Cannon Few</p>
-              <p>mixed by: Cannon Few</p>
-              <p>mastered by: Tom Waltz</p>
-            </div>
-          </div>
-           */}
+  const backgroundImage = {
+    backgroundImage: isTabletUp ? "url('/assets/bleu-tapes.png')" : "url('/assets/play-page-mobile.jpg')",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
+  const backgroundImageInner = {
+    backgroundImage: "url('/assets/bleu-tracks.png')",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
 
-          <div className="h100 none lg:block" style={backgroundImageInner}></div>
-        </div>
-      </a>
-    );
-  }
+  return (
+    <a
+      href="https://open.spotify.com/album/0p6BK5gxpxydQPG5HsyOIl?si=WUH1JklSRBmXB7x3AgZdKQ"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="BleuTapes BleuTapes__bg p1" style={backgroundImage}>
+        <div className="BleuTapes__inner h100 none lg:block" style={backgroundImageInner}></div>
+      </div>
+    </a>
+  );
+
 }
+export default BleuTapes;
---
+++ src/utils/breakpoints.js
@@ -0,0 +1,10 @@
+// NOTE: We also have CSS breakpoints defined in scss files. They should probably match.
+
+const breakpoints = {
+  'mobile': "(min-width: 320px)",
+  'tablet': "(min-width: 720px)",
+  'desktop': "(min-width: 1024px)",
+  'desktop-max': "(min-width: 1440px)"
+};
+
+export default breakpoints;
